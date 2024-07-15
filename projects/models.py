c80a0f7684d16067bad3ce79912d3727b0b010ca
Tom O'Leary <tr.oleary@outlook.com>
2024-07-15 12:58:39 -0400

    add date created to projects

--- projects/models.py
+++ projects/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class Project(models.Model):
+    date_created = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=100)
     description = models.TextField()
     technology = models.CharField(max_length=200)
