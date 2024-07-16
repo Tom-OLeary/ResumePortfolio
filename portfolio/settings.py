94eabfbc131cbac993b0537b8edaa7e8690dfcf9
Tom O'Leary <tr.oleary@outlook.com>
2024-07-16 15:55:08 -0400

    update settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -89,20 +89,13 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
-DB_PASSWORD = os.getenv("DB_PASSWORD")
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'troleary$default',
-        'USER': 'troleary',
-        'PASSWORD': DB_PASSWORD,
+        'USER': 'xxxxxxxxx',
+        'PASSWORD': "xxxxxxxxxx",
         'HOST': 'troleary.mysql.pythonanywhere-services.com',
     }
 }
@@ -143,6 +136,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
